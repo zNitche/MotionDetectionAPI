c26e1ea5af16b0e9ca88decd2f7ccd7dc96410c7
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-03-28 15:51:45 +0200

    added logging notification messages

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, current_app, request, jsonify
 from config import ApiConfig
+import log_utils
 import json
 from datetime import datetime
 
@@ -15,7 +16,10 @@ def notify():
         parsed_data = json.loads(data.decode("utf-8"))
 
         if parsed_data[ApiConfig.TOKEN_KEY_NAME] == ApiConfig.AUTH_TOKEN:
-            current_app.motion_notifier.send_message(f"[{datetime.now()}]"
-                                                     f" {parsed_data[ApiConfig.SENSOR_NAME_KEY_NAME]}")
+            notification_message = parsed_data[ApiConfig.SENSOR_NAME_KEY_NAME]
+
+            current_app.motion_notifier.send_message(f"[{datetime.now()}] {notification_message}")
+
+            log_utils.log_message(notification_message)
 
     return jsonify(status="OK")
